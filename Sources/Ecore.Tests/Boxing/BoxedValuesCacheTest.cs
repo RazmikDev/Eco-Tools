7b3ee2708535fc87ffedd49f124ebc7f1ba384fc
Razmik Seysyan <razmikdev@outlook.com>
2015-06-04 14:35:45 +0300

    Adds first-view tests for Box() and BoxGeneric() extension methods

---
+++ Sources/Ecore.Tests/Boxing/BoxedValuesCacheTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Eco.Boxing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eco.Tests.Boxing
+{
+	[TestClass]
+	public sealed class BoxedValuesCacheTest
+	{
+		[TestMethod]
+		public void StructureBox_BoxSameValueTwiceTest()
+		{
+			const Int32 x = 5;
+			var xBoxed1 = x.Box();
+			var xBoxed2 = x.Box();
+			Assert.AreEqual(xBoxed1, xBoxed2);
+			Assert.AreSame(xBoxed1, xBoxed2);
+		}
+
+		[TestMethod]
+		public void StructureBox_BoxDifferentValuesTest()
+		{
+			const Int32 a = 5;
+			const Int32 b = 10;
+			var aBoxed = a.Box();
+			var bBoxed = b.Box();
+			Assert.AreNotEqual(aBoxed, bBoxed);
+			Assert.AreNotSame(aBoxed, bBoxed);
+		}
+
+		[TestMethod]
+		public void StructureBoxGeneric_BoxSameValueTwiceTest()
+		{
+			const Int32 x = 5;
+			var xBoxed1 = x.BoxGeneric();
+			var xBoxed2 = x.BoxGeneric();
+			Assert.AreEqual(xBoxed1, xBoxed2);
+			Assert.AreSame(xBoxed1, xBoxed2);
+		}
+
+		[TestMethod]
+		public void StructureBoxGeneric_BoxDifferentValuesTest()
+		{
+			const Int32 a = 5;
+			const Int32 b = 10;
+			var aBoxed = a.BoxGeneric();
+			var bBoxed = b.BoxGeneric();
+			Assert.AreNotEqual(aBoxed, bBoxed);
+			Assert.AreNotSame(aBoxed, bBoxed);
+		}
+	}
+}
