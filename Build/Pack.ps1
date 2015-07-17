64ee52b19a95c84a5106f7ec157177f56942f7be
Razmik Seysyan <seysyan@gmail.com>
2015-07-17 20:30:01 +0300

    Updates nuget build script to support version as parameter and cleanup folder before build:

--- Build/Pack.ps1
+++ Build/Pack.ps1
@@ -1,7 +1,14 @@
+param
+(
+    [string] $version = "0.0.1"
+)
+
+ls -Filter *.nupkg | rm -Force
+
 $nuget = Join-Path $PSScriptRoot "\NuGet-Signed.exe" -Resolve
 $nuspecs = ls -Path $PSScriptRoot -Filter *.nuspec
 
 foreach($nuspec in $nuspecs)
 {
-    & $nuget Pack $nuspec
+    & $nuget Pack $nuspec -version $version -NonInteractive
 }
